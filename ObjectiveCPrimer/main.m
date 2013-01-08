880c3a28bb888c5686d3e5fe526f9e6300bb1007
Yong Bakos <ybakos@humanoriented.com>
2013-01-08 07:20:53 -0700

    Initial Commit

---
+++ ObjectiveCPrimer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ObjectiveCPrimer
+//
+//  Created by Yong Bakos on 1/8/13.
+//  Copyright (c) 2013 Yong Bakos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
