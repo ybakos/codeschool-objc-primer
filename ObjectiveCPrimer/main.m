06054d74a72dbe432b1285d39450f814bfd87e6c
Yong Bakos <ybakos@humanoriented.com>
2013-01-08 07:22:50 -0700

    Cleaning up initial source file template.

--- ObjectiveCPrimer/main.m
+++ ObjectiveCPrimer/main.m
@@ -8,12 +8,10 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[])
-{
+int main(int argc, const char * argv[]) {
 
     @autoreleasepool {
         
-        // insert code here...
         NSLog(@"Hello, World!");
         
     }
